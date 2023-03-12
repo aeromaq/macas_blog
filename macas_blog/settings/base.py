8f72af06844069122752361ea18b89e727c7824f
Aironne Mark Quintela <aironne.quintela25@gmail.com>
2023-03-13 03:27:10 +0800

    installed whitenoise

--- macas_blog/settings/base.py
+++ macas_blog/settings/base.py
@@ -146,10 +146,10 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = "root/home/macas_blog/static/"
+STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "root/home/macas_blog/media/"
+MEDIA_URL = "/media/"
 
 # Wagtail settings
 
