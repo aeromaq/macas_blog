6a85e1abb30baa5b7a2c0c77f5f2fdaa3c5d9eea
Aironne Mark Quintela <aironne.quintela25@gmail.com>
2023-03-13 21:52:50 +0800

    STATIC settings fix

--- macas_blog/settings/base.py
+++ macas_blog/settings/base.py
@@ -157,7 +157,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
