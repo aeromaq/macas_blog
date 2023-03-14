9e1c986cf4ffe5970224a0c963d469d4195fd388
Aironne Mark Quintela <aironne.quintela25@gmail.com>
2023-03-15 01:26:30 +0800

    debug=False

--- macas_blog/settings/dev.py
+++ macas_blog/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-byd+#(-wq+-@qu#$0zy)lh_k+79tm*mmzw^g$w(4d#3_9=!6gy"
--- macas_blog/settings/production.py
+++ macas_blog/settings/production.py
@@ -10,3 +10,4 @@ CSRF_TRUSTED_ORIGINS = ['https://*.macas.tech','https://macas.tech','https://*.1
 ALLOWED_HOSTS = ['https://*.macas.tech', 'https://macas.tech', 'www.macas.tech', 'macas.tech', 'https://*.127.0.0.1',
                  '206.189.153.198']
 USE_X_FORWARDED_PORT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
