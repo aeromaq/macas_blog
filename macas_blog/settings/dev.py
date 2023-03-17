6411a6e3ce34136e9e51d6a1d6376a2d585d5c88
aeromaq <63344255+aeromaq@users.noreply.github.com>
2023-03-17 21:37:09 +0800

    Added css

--- macas_blog/settings/dev.py
+++ macas_blog/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-byd+#(-wq+-@qu#$0zy)lh_k+79tm*mmzw^g$w(4d#3_9=!6gy"
@@ -10,9 +10,8 @@ SECRET_KEY = "django-insecure-byd+#(-wq+-@qu#$0zy)lh_k+79tm*mmzw^g$w(4d#3_9=!6gy
 CSRF_TRUSTED_ORIGINS = ['https://*.macas.tech', 'https://macas.tech', 'https://*.127.0.0.1', 'http://206.189.153.198']
 ALLOWED_HOSTS = ['https://*.macas.tech', 'https://macas.tech', 'www.macas.tech', 'macas.tech', 'https://*.127.0.0.1',
                  '206.189.153.198', '127.0.0.1:8000', '127.0.0.1']
-USE_X_FORWARDED_PORT = True
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 try:
     from .local import *
